fbae8c14a2dc8bd70e6f6129b93912d9a4388bf0
Gnutyud <nguyenduytungtd3@gmail.com>
2023-05-27 12:03:50 +0700

    implement todo page UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
+            {/* <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
                 Just the basics - Everything you need to know to set up your
                 database and authentication.
               </div>
-            </Link>
+            </Link> */}
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
@@ -41,6 +41,15 @@ const Home: NextPage = () => {
               <div className="text-lg">
                 Learn more about Create T3 App, the libraries it uses, and how
                 to deploy it.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/todo"
+            >
+              <h3 className="text-2xl font-bold">Todo List →</h3>
+              <div className="text-lg">
+                Go to this page to see what you have to do today
               </div>
             </Link>
           </div>
@@ -63,7 +72,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
@@ -74,7 +83,11 @@ const AuthShowcase: React.FC = () => {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+        onClick={
+          sessionData
+            ? () => void signOut()
+            : () => void signIn("github", { callbackUrl: "/todo" })
+        }
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
---
+++ src/pages/todo.tsx
@@ -0,0 +1,126 @@
+import { type NextPage } from "next";
+import Link from "next/link";
+
+const Todo: NextPage = () => {
+  return (
+    <div className="h-screen w-screen bg-gray-100 p-4">
+      <Link
+        href="/"
+        className="inline-flex items-center rounded-md bg-indigo-500 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-600"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mr-2 h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"
+          />
+        </svg>
+        Back to home page
+      </Link>
+      <div className="mt-10 flex items-center justify-center font-medium">
+        <div className="flex h-full flex-grow items-center justify-center  text-gray-600">
+          <div className="w-96 max-w-full rounded-lg bg-white p-8 shadow-lg">
+            <div className="mb-6 flex items-center">
+              <svg
+                className="h-8 w-8 stroke-current text-indigo-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                />
+              </svg>
+              <h4 className="ml-3 text-lg font-semibold">Todo List</h4>
+            </div>
+            <TodoItem name="nau com" id={"1"} complete={false} />
+            <TodoItem name="rau bat" id={"2"} complete={false} />
+            <TodoItem name="giat quan ao" id={"3"} complete={false} />
+            <TodoItem name="learning English 1h" id={"4"} complete={false} />
+            <TodoItem name="ve sinh ca nhan" id={"1"} complete={true} />
+            <div className="mt-4 flex items-center justify-between gap-2">
+              <input
+                className="ml-4 h-8 w-full flex-grow bg-transparent font-medium focus:outline-none"
+                type="text"
+                placeholder="add a new task"
+              />
+              <button
+                type="button"
+                className="mr-2 inline-flex items-center rounded-full bg-blue-700 p-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                <svg
+                  className="h-5 w-5 fill-current text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+                <span className="sr-only">add button</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TodoItem: NextPage<{
+  name: string;
+  complete: boolean;
+  id: string;
+}> = ({ name, complete, id }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <input className="hidden" type="checkbox" id={id} checked={complete} />
+        <label
+          className="flex h-10 cursor-pointer items-center rounded px-2 hover:bg-gray-100"
+          htmlFor="task_5"
+        >
+          <span className="flex h-5 w-5 items-center justify-center rounded-full border-2 border-gray-300 text-transparent">
+            <svg
+              className="h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+          <span className="ml-4 text-sm">{name}</span>
+        </label>
+      </div>
+      <button
+        type="button"
+        className="font-small rounded-lg bg-transparent px-5 py-2.5 text-sm text-red-500  hover:text-red-600"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Todo;
